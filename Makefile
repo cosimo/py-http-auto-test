f7ffd1e738812e574da992d7b6e7d14f11ff1ee2
Cosimo Streppone <cosimo@kahoot.com>
2023-08-29 18:24:08 +0200

    Added setuptools files to prepare upload to pypi

--- Makefile
+++ Makefile
@@ -1,3 +1,12 @@
+.PHONY: dist
+
+dist:
+	rm -rf dist build
+	python setup.py sdist bdist_wheel
+
+release:
+	python -m twine upload dist/*
+
 test:
 	pytest-3 -v ./spec/test*.yaml --capture=no
 
--- setup.py
+++ setup.py
@@ -1,13 +1,33 @@
-#!/usr/bin/env python
+import os
 
-from distutils.core import setup
+from setuptools import setup
+
+from http_test import __version__
+
+
+def read(filename):
+    fullpath = os.path.join(os.path.dirname(__file__), filename)
+    return open(fullpath, "r", encoding="utf-8").read()
+
 
 setup(
     name="py-http-test",
-    version="1.0",
+    version=__version__,
     description="Python HTTP automated testing facility",
+    long_description=read("README.md"),
     author="Cosimo Streppone",
     author_email="cosimo@streppone.it",
     url="https://github.com/cosimo/py-http-test",
     packages=["http_test"],
+    license="MIT",
+    install_requires=open("requirements.txt").readlines(),
+    python_requires=">=3.8",
+    scripts=["bin/http-test-runner.py"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Utilities",
+        "Operating System :: OS Independent",
+    ],
 )
