e06162ab4c6dd9a02552a8f334f1b4223b1fcae5
Cosimo Streppone <cosimo@kahoot.com>
2023-08-29 14:44:52 +0200

    Not sure what I'm doing here, but ok

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="py-http-test",
+    version="1.0",
+    description="Python HTTP automated testing facility",
+    author="Cosimo Streppone",
+    author_email="cosimo@streppone.it",
+    url="https://github.com/cosimo/py-http-test",
+    packages=["http_test"],
+)
